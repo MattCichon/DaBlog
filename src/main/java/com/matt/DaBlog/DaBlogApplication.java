3a10174e702bb75da89a9afe4a4aab28dea7e748
Matt Cichon <73495007+MattCichon@users.noreply.github.com>
2020-11-29 12:23:42 -0500

    Add files via upload

---
+++ src/main/java/com/matt/DaBlog/DaBlogApplication.java
@@ -0,0 +1,13 @@
+package com.matt.DaBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matt/DaBlog/DaBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matt.DaBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
